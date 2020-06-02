9ab41492fe83b3d7453188d0aca3450425ec04f1
Gustavochagass <gustavochagasb@gmail.com>
2020-06-02 19:23:09 -0300

    knex

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listg');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    response.json({
-        name: 'Gustavo'
-    });
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
